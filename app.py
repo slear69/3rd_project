ea106c2c76b1196c4e7d95e35068694c3ab559ae
hi_frombg:)ngl <dimitarp758@gmail.com>
2026-01-15 09:21:13 +0200

    Initialize Streamlit app for favorite things survey

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+st.title(" Любими неща класна анкета")
+# Инициализация на данните
+if "colors" not in st.session_state:
+    st.session_state.colors = {
+        "Червен": 0,
+        "Син": 0,
+        "Зелен": 0,
+        "Жълт": 0
+     }
+if "sports" not in st.session_state:
+    st.session_state.sports = {
+        "Футбол": 0,
+        "Баскетбол": 0,
+        "Волейбол": 0,
+        "Плуване": 0
+     }
+st.subheader("Избери любими неща")
+color = st.selectbox ("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox ("Любим спорт:", list(st.session_state.sports.keys()))
+
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport st.selectbox ("Любим спорт:", list(st.session_state.sports.keys()))
+if st.button("Запази избора"):
+    st.session_state.colors[color] += 1
+    st.session_state.sports [sport] += 1
+    st.success("Изборът е записан!")
+st.divider()
+st.subheader(" Резултати")
+# Графика за цветовете
+st.write("Любими цветове")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
+# Графика за спортовете
+st.write("Любими спортове")
+sports_df pd.DataFrame.from_dict(
+     st.session_state.sports, orient="index", columns=["Брой"]
+)
+st.bar_chart(sports_df)
