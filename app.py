2137a51696706198bed20aa8a368c321fe08b199
hi_frombg:)ngl <dimitarp758@gmail.com>
2026-01-15 09:42:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,22 @@ st.title(" Любими неща класна анкета")
 # Инициализация на данните
 if "colors" not in st.session_state:
     st.session_state.colors = {
-        "Червен": 0,
-        "Син": 0,
-        "Зелен": 0,
-        "Жълт": 0
+        "владо": 0,
+        "божо": 0,
+        "злати": 0,
+        "митко": 0
      }
 if "sports" not in st.session_state:
     st.session_state.sports = {
-        "Футбол": 0,
-        "Баскетбол": 0,
-        "Волейбол": 0,
-        "Плуване": 0
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0
      }
 st.subheader("Избери любими неща")
-color = st.selectbox ("Любим цвят:", list(st.session_state.colors.keys()))
-sport = st.selectbox ("Любим спорт:", list(st.session_state.sports.keys()))
+color = st.selectbox ("учиник:", list(st.session_state.colors.keys()))
+sport = st.selectbox ("оценка:", list(st.session_state.sports.keys()))
 
 if st.button("Запази избора"):
     st.session_state.colors[color] += 1
